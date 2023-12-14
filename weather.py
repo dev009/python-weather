38d7983e9b693c015a9fd7daf1fb59d17a39fe84
Dev Bibodi <devalbibodi@gmail.com>
2023-12-14 13:07:59 +0530

    First commit

---
+++ weather.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+def get_current_weather(city="Pune"):
+     request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city.strip()}&units=metric'
+     weather_data = requests.get(request_url).json()
+     return weather_data
+
+if __name__ == "__main__":
+     print("\n Get current weather conidtions \n")
+     city = input("\nPlease entery city name: ")
+    
+     if not bool(city.strip()):
+          city="pune"
+
+     weather_data = get_current_weather(city)
+     print("\n")
+     pprint(weather_data)
